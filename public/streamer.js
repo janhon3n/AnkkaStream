ed23e626f09f846ae52e9cd71414073a29b1935d
Mikael Janhonen <janhon3n@gmail.com>
2017-08-15 18:58:00 +0300

    Edit jsmpeg options

--- public/streamer.js
+++ public/streamer.js
@@ -1,3 +1,3 @@
 var canvas = document.getElementById('stream-canvas');
 var url = 'ws://'+document.location.hostname+':8082/';
-var player = new JSMpeg.Player(url, {canvas: canvas});
+var player = new JSMpeg.Player(url, {canvas: canvas, poster: "http://jiipeenetti.fi/images/stories/testit/ankka.jpg", autoplay:true});
