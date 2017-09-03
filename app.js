a667128f43f5f1262c115d037779e7df8e115041
miquel <miquel@minecraftserver>
2017-09-03 22:10:35 +0300

    Add socket io

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var moment = require('moment');
 var pug = require('pug');
 var express = require('express');
 var app = express();
+var io = require('socket.io')(app);
 app.set('view engine', 'pug');
 
 app.use(express.static('public'));
@@ -20,11 +21,22 @@ var twitterError = function(err, response, body){
 }
 var twitterSuccess = function(data){
     var twitterData = JSON.parse(data);
+    var newTweets = [];
     twitterData.statuses.forEach((s) => {
         s.created_at = moment(new Date(s.created_at.replace(/^\w+ (\w+) (\d+) ([\d:]+) \+0000 (\d+)$/,
                 "$1 $2 $4 $3 UTC"))).format('DD.MM.YYYY HH:mm');
+        if(tweetCache.statuses.some((cachedTweet) => {
+
+	})){
+		
+	}
     });
+    //TODO only update if data changed and send changes
     tweetCache = twitterData;
+    var clients = io.sockets.clients();
+    clients.forEach((c) => {
+        c.emit('tweetUpdate', 
+    });
     console.log('Tweets fetched from Twitter');
 }
 
@@ -55,3 +67,10 @@ function handleError(err){
 app.listen(3000, function() {
     console.log('App running on port 3000');
 })
+
+
+
+/* SOCKET IO STUFF */
+io.on('connection', function(socket){
+
+});
--- public/streamer.js
+++ public/streamer.js
@@ -1,4 +1,11 @@
 var canvas = document.getElementById('stream-canvas');
+
+var ctx = canvas.getContext("2d");
+ctx.font = "30px Arial";
+ctx.fillText("Hello World",10,50);
+
+
 var url = 'ws://'+document.location.hostname+':8082/';
-var player = new JSMpeg.Player(url, {canvas: canvas, poster: "http://jiipeenetti.fi/images/stories/testit/ankka.jpg", autoplay:true, disableGl:true});
+var player = new JSMpeg.Player(url, {canvas: canvas, autoplay:auto, disableGl:true});
 setInterval(() => {player.currentTime}, 1000);
+
